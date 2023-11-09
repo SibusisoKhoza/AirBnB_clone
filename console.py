95664b5daf66dd7c972e2f3bf09b1eab9d84abdd
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-11-09 16:33:54 +0200

    Add asset folders
    -Add "create" function prototype
    
    2. Unittests /tests
    3. BaseModel models/base_model.py, models/__init__.py, tests/

--- console.py
+++ console.py
@@ -6,17 +6,26 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """ Type quit to exit the program """
+        """ Type <quit> to exit the program """
         exit()
 
     def do_EOF(self, arg):
-        """ Type EOF to quit the program """
+        """ Type <EOF> to quit the program """
         print('')
         exit()
 
     def emptyline(self):
         """ Ignores empty lines entered by a user """
         pass
+
+    def do_create(self, arg):
+        """
+        Creates a new instance of a valid Class, saves it (to the JSON file) and prints the id.
+        Valid classes are: BaseModel, User, State, City, Amenity, Place, Review.
+        Usage:	create <class name>
+        """
+
+        
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
