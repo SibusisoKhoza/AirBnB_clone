53d6adcad12826a091b214105d5567ad5162d141
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-11-09 14:53:36 +0200

    Console 0.0.1
    -Add quit and EOF to exit the program
    -Add empty line to ignore users newlines

--- console.py
+++ console.py
@@ -1,9 +1,22 @@
+"""The Console Module"""
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """HBNBCommand class sub class of the CMD instance"""
+    """ HBNBCommand class, sub class of the CMD instance """
     prompt = '(hbnb) '
 
+    def do_quit(self, arg):
+        """ Type quit to exit the program """
+        exit()
+
+    def do_EOF(self, arg):
+        """ Type EOF to quit the program """
+        print('')
+        exit()
+
+    def emptyline(self):
+        """ Ignores empty lines entered by a user """
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
