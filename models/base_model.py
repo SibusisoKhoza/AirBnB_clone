3deeb0430ea3e1dd51d08425eb8c9c640545e625
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-11-09 17:54:32 +0200

    Test base Model

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,18 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    """ The BaseModel Class defines all common attributes and methods for other classes """
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
