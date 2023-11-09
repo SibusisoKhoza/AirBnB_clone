85f4d903a225424f4564b4c753cd68ea3c3eeb88
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-11-09 16:46:57 +0200

    Update execution path

---
+++ models/base_model.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Defines the class: BaseModel """
+from uuid import uuid4
+
+class BaseModel:
+    """ The BaseModel Class defines all common attributes and methods for other classes """
