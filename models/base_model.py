914a6c961e1a9056e57d82b2e061433ef24053ed
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-11-09 17:47:39 +0200

    Add BaseModel Class
    Public instance attributes:
    id: string - assign with an uuid when an instance is created:
    you can use uuid.uuid4() to generate unique id but don’t forget to convert to a string
    the goal is to have unique id for each BaseModel
    created_at: datetime - assign with the current datetime when an instance is created
    updated_at: datetime - assign with the current datetime when an instance is created and it will be updated every time you change your object
    __str__: should print: [<class name>] (<self.id>) <self.__dict__>
    Public instance methods:
    save(self): updates the public instance attribute updated_at with the current datetime
    to_dict(self): returns a dictionary containing all keys/values of __dict__ of the instance:
    by using self.__dict__, only instance attributes set will be returned
    a key __class__ must be added to this dictionary with the class name of the object
    created_at and updated_at must be converted to string object in ISO format:
    format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
    you can use isoformat() of datetime object
    This method will be the first piece of the serialization/deserialization process: create a dictionary representation with “simple object type” of our BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 """ Defines the class: BaseModel """
-from uuid import uuid4
+import uuid
+from datetime import datetime
 
 class BaseModel:
     """ The BaseModel Class defines all common attributes and methods for other classes """
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        model_dict = self.__dict__.copy()
+        model_dict['__class__'] = self.__class__.__name__
+        model_dict['created_at'] = self.created_at.isoformat()
+        model_dict['updated_at'] = self.updated_at.isoformat()
+        return model_dict
