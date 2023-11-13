b2fec0f4781431f55ad590943f40f5c8d31c0add
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-11-14 00:07:41 +0200

    Update console.py

--- console.py
+++ console.py
@@ -84,15 +84,14 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
 
     def do_all(self, arg):
-        """Prints all string representation of all instances"""
-        args = shlex.split(arg)
-        all_objects = storage.all()
-        if not args:
-            print([str(obj) for obj in all_objects.values()])
-        elif args[0] not in classes:
+        """ Method to print all instances """
+        if len(arg) == 0:
+            print([str(a) for a in storage.all().values()])
+        elif arg not in self.classes:
             print("** class doesn't exist **")
         else:
-            print([str(obj) for obj in classes[args[0]].all()])
+            print([str(a) for b, a in storage.all().items() if arg in b])
+
 
     def do_update(self, arg):
         """Updates an instance based on the class name and id"""
