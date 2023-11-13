034933fa82c18cf849f83171a803fe644a275907
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-11-14 00:42:04 +0200

    m

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 from models.base_model import BaseModel
-
+from models import storage
 
 class User(BaseModel):
     """User class that inherits from BaseModel"""
@@ -11,6 +11,5 @@ class User(BaseModel):
     @classmethod
     def count(cls):
         """Returns the number of instances of User"""
-        arr = [obj for obj in storage.all().values() if isinstance(obj, cls)]
-        user_instances = arr
-        return len(user_instances)
+        return sum(1 for obj in storage.all().values() if isinstance(obj, cls))
+
