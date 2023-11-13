0b004544288845ac2f29f5fd3aabefb41a73fa35
SibusisoKhozaCJ <sbusiso.khoza5@gmail.com>
2023-11-13 05:55:41 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -11,5 +11,6 @@ class User(BaseModel):
     @classmethod
     def count(cls):
         """Returns the number of instances of User"""
-        user_instances = [obj for obj in storage.all().values() if isinstance(obj, cls)]
+        arr = [obj for obj in storage.all().values() if isinstance(obj, cls)]
+        user_instances = arr
         return len(user_instances)
